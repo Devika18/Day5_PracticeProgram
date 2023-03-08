1715ea0a62a412aa11b34dfb07c6c8bac2dce0ca
devikakarpe <devikankarpe@gmail.com>
2023-03-08 17:41:24 +0530

    added logic for checking even odd

---
+++ src/EvenOdd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        System.out.println("Enter the number: ");
+        Scanner n = new Scanner(System.in);
+        int num = n.nextInt();
+        if(num%2 == 0) {
+            System.out.println(num+": is a Evan number");
+        }
+        else
+            System.out.println(num+": is a Odd number");
+    }
+}
